458133c5c3a54090733cb8d45abd39f9a8250c3d
PixNyb <contact@roelc.me>
2024-06-05 19:55:22 +0000

    Added jekyll

---
+++ profiles/src/jekyll/scripts/initialise-vscode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PROJECT_FOLDER=${PROJECT_FOLDER:-~/project}
+PROJECT_NAME=${PROJECT_NAME:-project}
+PROJECT_NAME=$(echo $PROJECT_NAME | sed 's/[^a-zA-Z0-9]/_/g')
+
+cd $PROJECT_FOLDER
+
+# If possible, run the Jekyll server
+if [[ -f "_config.yml" ]]; then
+	bundle install
+fi
+
+echo "alias serve='JEKYLL_ENV=production bundle exec jekyll serve --host 0.0.0.0 --drafts'" >>~/.bashrc
+
+echo "echo -e 'You are currently running a \033[1;36mJekyll\033[0m generic container.'" >>~/.bashrc
+echo "echo -e 'Useful commands:'" >>~/.bashrc
+echo "echo -e '  - \033[1;36mserve\033[0m: Run the Jekyll server'" >>~/.bashrc
