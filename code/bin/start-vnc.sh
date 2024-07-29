7afb15819e24e4e559cbc6f2546e0355f917db08
PixNyb <contact@roelc.me>
2024-07-29 12:14:52 +0000

    Swapped x11vnc out for openbox/turbovnc for performance reasons

--- code/bin/start-vnc.sh
+++ code/bin/start-vnc.sh
@@ -1,3 +1,2 @@
 rm -f /tmp/.X*-lock
-Xvfb ${DISPLAY} -screen 0 1920x1080x24 &
-x11vnc -shared -forever -display ${DISPLAY} -rfbport 5900 -nopw -xkb -noxrecord -noxfixes -noxdamage -xrandr -quiet -bg -o /tmp/x11vnc.log
+sudo /opt/TurboVNC/bin/vncserver -geometry 1920x1080 -depth 24 -rfbport 5900 -SecurityTypes None -xstartup openbox $DISPLAY
