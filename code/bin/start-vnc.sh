de7d30543b1616e4c314e5385c4f8faa686f7b32
PixNyb <contact@roelc.me>
2024-07-29 13:21:28 +0000

    vnc shouldn't start as root, but as the programming user

--- code/bin/start-vnc.sh
+++ code/bin/start-vnc.sh
@@ -1,2 +1,2 @@
 rm -f /tmp/.X*-lock
-sudo /opt/TurboVNC/bin/vncserver -geometry 1920x1080 -depth 24 -rfbport 5900 -SecurityTypes None -xstartup openbox $DISPLAY
+/opt/TurboVNC/bin/vncserver -geometry 1920x1080 -depth 24 -rfbport 5900 -SecurityTypes None -xstartup openbox $DISPLAY
