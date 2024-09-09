4c14287e84764028aec9dfb9e991ac82785093d2
PixNyb <contact@roelc.me>
2024-09-09 08:35:02 +0000

    Added dind(?)

--- profiles/src/cloud/scripts/initialise-vscode.sh
+++ profiles/src/cloud/scripts/initialise-vscode.sh
@@ -4,4 +4,6 @@ PROJECT_FOLDER=${PROJECT_FOLDER:-~/project}
 PROJECT_NAME=${PROJECT_NAME:-project}
 PROJECT_NAME=$(echo $PROJECT_NAME | sed 's/[^a-zA-Z0-9]/_/g')
 
+sudo dockerd &
+
 echo "echo -e 'You are currently running a \033[1;36mCloud\033[0m generic container.'" >>~/.bashrc
