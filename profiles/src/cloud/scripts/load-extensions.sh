2f3a109e7cc2b81d2c3d193876118dd5abb9e5b7
PixNyb <contact@roelc.me>
2024-10-01 11:06:23 +0000

    Added golang extension

--- profiles/src/cloud/scripts/load-extensions.sh
+++ profiles/src/cloud/scripts/load-extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-extensions="hashicorp.terraform,ms-kubernetes-tools.vscode-kubernetes-tools,ms-azuretools.vscode-docker,ms-kubernetes-tools.vscode-aks-tools,redhat.vscode-openshift-connector,ms-kubernetes-tools.kind-vscode,inercia.vscode-k3d,gardener.vscode-gardener-tools"
+extensions="golang.go,hashicorp.terraform,ms-kubernetes-tools.vscode-kubernetes-tools,ms-azuretools.vscode-docker,ms-kubernetes-tools.vscode-aks-tools,redhat.vscode-openshift-connector,ms-kubernetes-tools.kind-vscode,inercia.vscode-k3d,gardener.vscode-gardener-tools"
 if [ -z "$EXTENSION_LIST" ]; then
 	EXTENSION_LIST=$extensions
 else
