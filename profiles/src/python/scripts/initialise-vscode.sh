cc6486a38e63f5a77ab5b5cc570ecec17f9858d7
PixNyb <contact@roelc.me>
2024-10-13 10:43:41 +0000

    Oops!

--- profiles/src/python/scripts/initialise-vscode.sh
+++ profiles/src/python/scripts/initialise-vscode.sh
@@ -13,4 +13,4 @@ source $PROJECT_FOLDER/.venv/bin/activate
 echo "echo -e 'You are currently running a \033[1;31mPython\033[0m generic container.'" >>~/.bashrc
 echo "echo -e 'Included scripts:'" >>~/.bashrc
 echo "echo -e '  - \033[1;34mactivate\033[0m: Activate a Python virtual environment'" >>~/.bashrc
-echo "echo -e '    - \033[1;90mUsage\033[0m: activate [-p <path>]' (default path: $PROJECT_FOLDER/.venv)" >>~/.bashrc
+echo "echo -e '    - \033[1;90mUsage\033[0m: activate [-p <path>] (default path: $PROJECT_FOLDER/.venv)'" >>~/.bashrc
