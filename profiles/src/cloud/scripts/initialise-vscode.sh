e77a1411495fcf45b8d5e6fe06d3cea0709c41c0
PixNyb <contact@roelc.me>
2025-02-11 10:41:12 +0000

    Added autocompletion support

--- profiles/src/cloud/scripts/initialise-vscode.sh
+++ profiles/src/cloud/scripts/initialise-vscode.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+helm completion bash | sudo tee /etc/bash_completion.d/helm > /dev/null
+minikube completion bash | sudo tee /etc/bash_completion.d/minikube > /dev/null
+k3d completion bash | sudo tee /etc/bash_completion.d/k3d > /dev/null
+kind completion bash | sudo tee /etc/bash_completion.d/kind > /dev/null
+
 PROJECT_FOLDER=${PROJECT_FOLDER:-~/project}
 PROJECT_NAME=${PROJECT_NAME:-project}
 PROJECT_NAME=$(echo $PROJECT_NAME | sed 's/[^a-zA-Z0-9]/_/g')
--- profiles/src/cpp/scripts/initialise-vscode.sh
+++ profiles/src/cpp/scripts/initialise-vscode.sh
@@ -6,7 +6,7 @@ PROJECT_NAME=$(echo $PROJECT_NAME | sed 's/[^a-zA-Z0-9]/_/g')
 
 # If the project contains a vcpkg.json file, install the dependencies
 if [[ -f vcpkg.json ]]; then
-    vcpkg install --triplet x64-linux 
+    vcpkg install --triplet x64-linux
 fi
 
 echo "echo -e 'You are currently running a \033[1;36mC++\033[0m generic container.'" >>~/.bashrc
